18fc37c1a56d6a2a51fd7672a498cd7a944eec2e
Raphael Mandel <raphael.k.mandel@gmail.com>
2024-09-17 13:34:46 -0400

    new options

--- lua/arkie/options.lua
+++ lua/arkie/options.lua
@@ -1,16 +1,43 @@
 --Shortcut
 local opt = vim.opt
 
---cmdheight
-opt.cmdheight = 1
+--Tabs
+TABWidth = 2
+opt.tabstop = TABWidth
+opt.shiftwidth = TABWidth
+opt.softtabstop = TABWidth
+opt.expandtab = true
+opt.autoindent = true
+opt.smartindent = true
+opt.wrap = false
 
---Scrolloff set
+--Search
+opt.incsearch = true
+opt.ignorecase = true
+opt.smartcase = true
+opt.hlsearch = false
+
+--Appearance
+opt.cursorline = true
+opt.signcolumn = "yes"
+opt.colorcolumn = "100"
+opt.cmdheight = 1
 opt.scrolloff = 10
+opt.completeopt = "menuone"
+
+--Behaivor
+opt.hidden = true
+opt.errorbells = false
+opt.swapfile = false
+opt.undofile = true
+opt.backspace = "indent,eol,start"
+opt.splitright = true
+opt.splitbelow = true
+opt.autochdir = false
+opt.modifiable = true
+opt.encoding = "utf-8"
 
 --StatusColumn Width
 SINGLENUMBERWIDTH = 4
 DUALNUMBERWIDTH = 7
 opt.numberwidth = DUALNUMBERWIDTH
-
---Highlight matching parentheses
-vim.cmd "hi MatchParen guifg=red gui=underline"
