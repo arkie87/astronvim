b7966561123dab5665ffe0018fb47ca9ef945d64
Raphael Mandel <raphael.k.mandel@gmail.com>
2024-09-17 16:49:01 -0400

    updated colorscheme

--- lua/arkie/colorscheme.lua
+++ lua/arkie/colorscheme.lua
@@ -9,37 +9,44 @@ local RED = "#FF0000"
 local PURPLE = "#ccaaFF"
 local BLACK = "#000000"
 
-local highlight = function(group, command) vim.cmd("hi! " .. group .. " " .. command) end
+local highlight = function(group, commands)
+  local str = group
+  for _, v in ipairs(commands) do
+    str = str .. " " .. v
+  end
+  vim.cmd("hi! " .. str)
+end
 
 --GUI
-highlight("MatchParen", "guifg=none gui=underline")
-highlight("CursorLine", "guibg=" .. DARKGREY)
-highlight("CursorLineNr", "guifg=" .. YELLOW)
-highlight("LineNr", "guifg=" .. GREY)
-highlight("WinSeparator", "guifg=" .. GREY)
-highlight("ColorColumn", "guibg=" .. DARKGREY)
+highlight("MatchParen", { "guifg=none", "gui=underline" })
+highlight("CursorLine", { "guibg=", DARKGREY })
+highlight("CursorLineNr", { "guifg=", YELLOW })
+highlight("LineNr", { "guifg=", GREY })
+highlight("WinSeparator", { "guifg=", GREY })
+highlight("ColorColumn", { "guibg=", DARKGREY })
 
 --Punctuations
-local PUNCTUATION = "guifg=" .. ORANGE
+local PUNCTUATION = { "guifg=", ORANGE }
 highlight("Operator", PUNCTUATION) --operators e.g. +=-
 highlight("@punctuation.bracket", PUNCTUATION) --e.g. [{()}]
 highlight("@punctuation.delimiter", PUNCTUATION) --e.g. :,.
+highlight("@punctuation.special", PUNCTUATION) --e.g. f-string {}
 
 --Comments
-highlight("Comment", "guifg=" .. YELLOW)
+highlight("Comment", { "guifg=", YELLOW })
 
 --Variables
-highlight("Identifier", "guifg=" .. GREY) --variables
-highlight("@property", "guifg=" .. GREY) --properties
+highlight("Identifier", { "guifg=", GREY }) --variables
+highlight("@property", { "guifg=", GREY }) --properties
 
 --Built-Ins
-local BUILTIN = "guifg=" .. YELLOW
-highlight("@type.builtin", BUILTIN .. " gui=bold,italic")
+local BUILTIN = { "guifg=", YELLOW }
+highlight("@type.builtin", BUILTIN)
 highlight("@function.builtin", BUILTIN)
 highlight("@attribute", BUILTIN) --decorator
 
 --Literals
-local LITERAL = "guifg=" .. PURPLE
+local LITERAL = { "guifg=", PURPLE }
 highlight("Constant", LITERAL)
 highlight("Character", LITERAL)
 highlight("String", LITERAL)
@@ -48,24 +55,24 @@ highlight("Float", LITERAL)
 highlight("Boolean", LITERAL)
 
 --Keywords
-local KEYWORD = " guifg=" .. BLUE .. " cterm=bold,italic"
+local KEYWORD = { " guifg=", BLUE }
 highlight("Keyword", KEYWORD)
 highlight("@keyword.function", KEYWORD)
 highlight("Include", KEYWORD) --Import
 highlight("Typedef", KEYWORD) --Class
 highlight("@variable.builtin", KEYWORD) --Self
-highlight("@variable.parameter", KEYWORD) --args, kwargs
+highlight("@variable.parameter", { "guifg=", YELLOW }) --args, kwargs
 highlight("Conditional", KEYWORD)
 highlight("Define", KEYWORD)
 
 --Functions
-local FUNCTION = " guifg=" .. GREEN .. " gui=bold"
+local FUNCTION = { "guifg=", GREEN }
 highlight("Type", FUNCTION)
 highlight("Function", FUNCTION)
 highlight("@constructor", FUNCTION)
 
 --Diagnostics
-highlight("DiagnosticVirtualTextError", "guifg=" .. RED .. " guibg=" .. BLACK)
-highlight("DiagnosticVirtualTextWarn", "guifg=" .. ORANGE .. " guibg=" .. BLACK)
-highlight("DiagnosticVirtualTextInfo", "guifg=" .. BLUE .. " guibg=" .. BLACK)
-highlight("DiagnosticVirtualTextHint", "guifg=" .. GREEN .. " guibg=" .. BLACK)
+highlight("DiagnosticVirtualTextError", { "guifg=", RED, "guibg=", BLACK })
+highlight("DiagnosticVirtualTextWarn", { "guifg=", ORANGE, "guibg=", BLACK })
+highlight("DiagnosticVirtualTextInfo", { "guifg=", BLUE, "guibg=", BLACK })
+highlight("DiagnosticVirtualTextHint", { "guifg=", GREEN, "guibg=", BLACK })
