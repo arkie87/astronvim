4194f6c0ea20d5f231a8089e058c05d8c0d0a859
Raphael <Raphael.K.Mandel@gmail.com>
2024-09-17 22:31:52 -0400

    added autocommands.lua

---
+++ lua/arkie/autocommands.lua
@@ -0,0 +1,5 @@
+--Highlighted Yank
+vim.api.nvim_create_autocmd("TextYankPost", {
+  pattern = "*",
+  callback = function() vim.highlight.on_yank { higroup = "HighlightedYank", timeout = 200 } end,
+})
