e379cc84a60cd2bd8e4e3a4071293848ae72b025
arkie87 <raphael.k.mandel@gmail.com>
2024-09-26 07:32:45 -0400

    tree sitter text objects

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -5,6 +5,7 @@
 ---@type LazySpec
 return {
   "nvim-treesitter/nvim-treesitter",
+  dependencies = "nvim-treesitter/nvim-treesitter-textobjects",
   opts = {
     ensure_installed = {
       "lua",
