d7c17c31607f4ec1caef582775dae879098a0ae8
arkie87 <raphael.k.mandel@gmail.com>
2024-09-29 15:06:43 -0400

    commented autocommands

--- lua/arkie/autocommands.lua
+++ lua/arkie/autocommands.lua
@@ -3,3 +3,13 @@ vim.api.nvim_create_autocmd("TextYankPost", {
   pattern = "*",
   callback = function() vim.highlight.on_yank { higroup = "HighlightedYank", timeout = 200 } end,
 })
+
+-- vim.api.nvim_create_autocmd("BufEnter", {
+--   pattern = "*",
+--   callback = function() print "focus gain" end,
+-- })
+--
+-- vim.api.nvim_create_autocmd("BufLeave", {
+--   pattern = "*",
+--   callback = function() print "focus lost" end,
+-- })
